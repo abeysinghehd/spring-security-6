148f59bca2280bd0b029349cf3ce6b351f01cd48
Hashan Darshana Abesinghe <abeysinghe.hd@gmail.com>
2024-04-07 14:17:20 +0800

    Initial commit

---
+++ src/main/java/abeysinghe/hd/springsecurity6demo/SpringSecurity6DemoApplication.java
@@ -0,0 +1,13 @@
+package abeysinghe.hd.springsecurity6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6DemoApplication.class, args);
+	}
+
+}
